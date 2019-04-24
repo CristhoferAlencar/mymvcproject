925e22274caa08dda31a4ddb89d14de00b05099d
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-04-24 14:38:39 -0300

    Created routing table

--- Core/Router.php
+++ Core/Router.php
@@ -5,4 +5,13 @@
  */
  class Router{
 
+    protected $routes = [];
+
+    public function add($route, $params){
+        $this->routes[$route] = $params;
+    }
+
+    public function getRoutes(){
+        return $this->routes;
+    }
  }
