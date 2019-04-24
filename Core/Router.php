74d0b886b096dc8b3901d9aa3c4c528f8c731b55
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-04-24 15:14:37 -0300

    Created basic match function for routing table

--- Core/Router.php
+++ Core/Router.php
@@ -6,6 +6,8 @@
  class Router{
 
     protected $routes = [];
+    
+    protected $params = [];
 
     public function add($route, $params){
         $this->routes[$route] = $params;
@@ -13,5 +15,18 @@
 
     public function getRoutes(){
         return $this->routes;
+    }
+
+    public function match($url){
+        foreach($this->routes as $route => $params){
+            if($url == $route){
+                $this->params = $params;
+                return true;
+            }
+        }
+    }
+
+    public function getParams(){
+        return $this->params;
     }
  }
--- public/index.php
+++ public/index.php
@@ -17,6 +17,17 @@ $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'POsts', 'action' => 'new']);
 
 // Display the routing table
-echo '<pre>';
+/* echo '<pre>';
 var_dump($router->getRoutes());
-echo '</pre>';
+echo '</pre>'; */
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url)){
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+}else{
+    echo "No route found for URL '$url'";
+}
