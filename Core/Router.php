a5814203dd0549728d92b2a09ea718307661aa14
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-04-26 07:03:40 -0300

    Created dispatch function in Router file

--- Core/Router.php
+++ Core/Router.php
@@ -5,10 +5,18 @@
  */
  class Router{
 
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
     protected $routes = [];
     
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
     protected $params = [];
-
+    
     /**
      * Add a route to the routing table
      * 
@@ -18,13 +26,12 @@
      * @return void
      */
     public function add($route, $params = []){
-        
         // Convert the route to a regular expression: escape forward slashes
         $route = preg_replace('/\//', '\\/', $route);
-    
+        
         // Convert variables e.g. {controller}
         $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
-
+        
         // Convert variables with custom regular expressions e.g. {id:/d+}
         $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
         
@@ -33,11 +40,16 @@
         
         $this->routes[$route] = $params;
     }
-
+    
+    /**
+     * Get all the routes from the routing table
+     * 
+     * @return array
+     */
     public function getRoutes(){
         return $this->routes;
     }
-
+    
     /**
      * Match the route to the routes in the routing table, setting the $params
      * property if a route is found
@@ -64,8 +76,70 @@
         
         return false;
     }
-
+    
+    /**
+     * Get the currently matched parameters
+     * 
+     * @return array
+     */
     public function getParams(){
         return $this->params;
     }
+
+    /**
+     * Dispatch the route, creating the controller object and running the
+     * action method
+     *
+     * @param string $url The route URL
+     *
+     * @return void
+     */
+    public function dispatch($url){
+        if($this->match($url)){
+            $controller = $this->params['controller'];
+            $controller = $this->convertToStudlyCaps($controller);
+            
+            if(class_exists($controller)){
+                $controller_object = new $controller();
+                
+                $action = $this->params['action'];
+                $action = $this->convertToCamelCase($action);
+                
+                if(is_callable([$controller_object, $action])){
+                    $controller_object->$action();
+                }else{
+                    echo "Method $action (in controller $controller) not found";
+                }
+            }else{
+                echo "Controller class $controller not found";
+            }
+        }else{
+            echo "No route matched.";
+        }
+    }
+    
+    /**
+     * Convert the string with hyphens to StudlyCaps,
+     * e.g. post-authors => PostAuthors
+     * 
+     * @param $string The string to convert
+     * 
+     * @return string
+     */
+    protected function convertToStudlyCaps($string){
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+    
+    /**
+     * Convert the string with hyphens to camelCase,
+     * e.g. add-new => addNew
+     * 
+     * @param string $string The string to convert
+     * 
+     * @return string
+     */
+    protected function convertToCamelCase($string){
+        return lcfirst($this->convertToStudlyCaps($string));
+    }
+
  }
