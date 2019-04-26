5de68a276295ec7f59e91d4cbe0d595833b36675
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-04-26 08:11:15 -0300

    Added action filters to the controllers

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -8,7 +8,7 @@ class Posts extends \Core\Controller{
      * 
      * @return void
      */
-    public function index(){
+    public function indexAction(){
         echo "Hello from the index action in the Posts controller!";
         echo "<p>Query string parameters: <pre>" .
          htmlspecialchars(print_r($_GET, true)) . "</pre></p>";
@@ -19,7 +19,7 @@ class Posts extends \Core\Controller{
      * 
      * @return void
      */
-    public function addNew(){
+    public function addNewAction(){
         echo "Hello from the addNew action in Posts controller";
     }
 
@@ -28,7 +28,7 @@ class Posts extends \Core\Controller{
      * 
      * @return void
      */
-    public function edit(){
+    public function editAction(){
         echo 'Hello from the edit action in the Posts controller!';
         echo '<p>Route parameters: <pre>' . htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
     }
