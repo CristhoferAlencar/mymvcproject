0a27904784e827cb4e2413b4d57618c46593a653
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-08 14:36:39 -0300

    Optimised database connection

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -6,7 +6,7 @@ use PDO;
 /* 
  * Post model
  */
-class Post{
+class Post extends \Core\Model{
     
     /**
      * Get all the posts as an associative array
@@ -15,13 +15,8 @@ class Post{
      */
     public static function getAll(){
 
-        $host = 'localhost';
-        $dbname = 'curso_mymvcproject';
-        $username = 'root';
-        $password = '';
-        
         try{
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $db = static::getDB();
             
             $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
