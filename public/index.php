ced8c46f93347b8d90afebdcca385509df1ba12f
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-08 13:46:31 -0300

    Added psr4 autoloading

--- public/index.php
+++ public/index.php
@@ -15,17 +15,6 @@ require '../vendor/autoload.php';
 Twig_Autoloader::register();
 
  /**
-  * Autoloader
-  */
-spl_autoload_register(function ($class){
-    $root = dirname(__DIR__); // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if(is_readable($file)){
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
-
- /**
   * Routing
   */
 //require '../Core/Router.php';
