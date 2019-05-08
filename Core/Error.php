980a549ff244ec2a1a502e4e65a4904e3ff49488
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-08 15:52:29 -0300

    Categorised different types of error

--- Core/Error.php
+++ Core/Error.php
@@ -29,6 +29,13 @@ class Error{
      * @return void
      */
     public static function exceptionHandler($exception){
+        // Code is 404 (not found) or 500 (general error)
+        $code = $exception->getCode();
+        if($code != 404){
+            $code = 500;
+        }
+        http_response_code($code);
+        
         if(\App\Config::SHOW_ERRORS){
             echo "<h1>Fatal error</h1>";
             echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
@@ -45,7 +52,11 @@ class Error{
             $message .= "\nThrown in '" . $exception->getFile() . "' on line " . $exception->getLine();
             
             error_log($message);
-            echo "<h1>An error occurred</h1>";
+            if($code == 404){
+                echo "<h1>Page not found</h1>";
+            }else{
+                echo "<h1>An error occurred</h1>";
+            }
         }
     }
 }
