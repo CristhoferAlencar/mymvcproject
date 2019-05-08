b96bfa17fb05da177bf801e8376fe3fceba65594
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-08 14:48:38 -0300

    Created separete file for application settings

--- Core/Model.php
+++ Core/Model.php
@@ -2,6 +2,7 @@
 namespace Core;
 
 use PDO;
+use App\Config;
 
 /* 
  * Base model
@@ -16,13 +17,9 @@ abstract class Model{
         static $db = null;
         
         if($db === null){
-            $host = 'localhost';
-            $dbname = 'curso_mymvcproject';
-            $username = 'root';
-            $password = '';
-
             try{
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+                $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
             } catch (PDOException $e){
                 echo $e->getMessage();
             }
