efd420ffe25176d05e59e8aca347ed5f1c204cb7
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-09 15:46:43 -0300

    Validating data before saving

--- App/Models/User.php
+++ App/Models/User.php
@@ -9,6 +9,13 @@ use PDO;
 class User extends \Core\Model{
     
     /**
+     * Error messages
+     * 
+     * @var array
+     */
+    public $errors = [];
+    
+    /**
      * Class constructor
      *
      * @param array $data Initial property values
@@ -27,18 +34,79 @@ class User extends \Core\Model{
      * @return void
      */
     public function save(){
-        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+        $this->validate();
+        
+        if(empty($this->errors)){
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
 
-        $sql = 'INSERT INTO users (name, email, password_hash) 
-                VALUES (:name, :email, :password_hash)';
+            $sql = 'INSERT INTO users (name, email, password_hash) 
+                    VALUES (:name, :email, :password_hash)';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash',  $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+
+        return false;
+    }
+
+    /**
+     * Validate current property values, adding validation error messages to the errors array property
+     * 
+     * @return void
+     */
+    public function validate(){
+        // Name
+        if($this->name == ''){
+            $this->errors[] = 'Name is required';
+        }
+        
+        // Email address
+        if(filter_var($this->email, FILTER_VALIDATE_EMAIL) === false){
+            $this->errors[] = 'Invalid email';
+        }
+
+        if($this->emailExists($this->email)){
+            $this->errors[] = 'Email already taken';
+        }
+
+        // Password
+        if(strlen($this->password) < 6){
+            $this->errors[] = 'Please enter at least 6 characters for the password';
+        }
+        
+        if(preg_match('/.*[a-z]+.*/i', $this->password) == 0){
+            $this->errors[] = 'Password needs at least one letter';
+        }
+
+        if(preg_match('/.*\d+.*/i', $this->password) == 0){
+            $this->errors[] = 'Password needs at least one number';
+        }
+    }
+
+    /**
+     * See if a user record already exists with the specified email
+     * 
+     * @param string $email email address to search for
+     * @param string $ignore_id Return false anyway if the record found has this ID
+     * 
+     * @return boolean True if a record already exists with the specified email, false otherwise
+     */
+    public static function emailExists($email){
+        $sql = 'SELECT * FROM users WHERE email = :email';
 
         $db = static::getDB();
         $stmt = $db->prepare($sql);
 
-        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $stmt->bindValue(':password_hash',  $password_hash, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
 
         $stmt->execute();
+
+        return $stmt->fetch() !== false;
     }
 }
