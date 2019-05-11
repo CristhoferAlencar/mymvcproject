7fbac0ea2f204cf23139932a55805a2ce8fd5277
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-10 22:46:32 -0300

    Created session handler and logout function

--- public/index.php
+++ public/index.php
@@ -16,6 +16,11 @@ error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
 
+/**
+ * Session
+ */
+session_start();
+
  /**
   * Routing
   */
@@ -24,6 +29,7 @@ $router = new Core\Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
+$router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
 $router->add('{controller}/{action}');
 //$router->add('{controller}/{id:\d+}/{action}');
 //$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
