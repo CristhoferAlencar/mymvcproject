44f04df3833ff5cc889b7909c1b2ba4caf159f5f
CristhoferAlencar <cristhofer-alencar@hotmail.com>
2019-05-12 01:19:11 -0300

    Added user profile function

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -9,23 +9,6 @@ use \Core\View;
 class Home extends \Core\Controller{
 
     /**
-     * Before filter
-     * 
-     * @return void
-     */
-    protected function before(){
-        //return false;
-    }
-    
-    /**
-     * After filter
-     * 
-     * @return void
-     */
-    protected function after(){
-    }
-     
-    /**
      * Show the index page
      * @return void
      */
